bf40e4fdfaf767e09b566bd4ed64d9fdf2be7bf3
Avinash Mutham <avinashmutham@Avinashs-MacBook-Pro.local>
2023-08-23 09:59:18 +0530

    practice

---
+++ project2/script.js
@@ -0,0 +1,8 @@
+function changeColor(){
+    var hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+
+    var hexcode = '';
+    for(var i=o;i < 6; i++)
+    console.log(Math.random() * hex_numbers.length)
+
+}
